37c0b89981d2c85189796688de039b7517efd72f
soeunkk <luckyber1@naver.com>
2022-08-21 00:37:48 +0900

    [1주차-숙제] initial project

---
+++ week01_hw/src/test/java/com/example/week01_hw/Week01HwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week01_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week01HwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
