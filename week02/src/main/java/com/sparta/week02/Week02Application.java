eb9e9685d7ac988bcaf33750de35dc7cbd29085a
soeunkk <luckyber1@naver.com>
2022-08-21 01:37:01 +0900

    [2주차] JPA 사용하기

--- week02/src/main/java/com/sparta/week02/Week02Application.java
+++ week02/src/main/java/com/sparta/week02/Week02Application.java
@@ -1,7 +1,14 @@
 package com.sparta.week02;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sparta.week02.domain.Course;
+import com.sparta.week02.domain.CourseRepository;
 
 @SpringBootApplication
 public class Week02Application {
@@ -10,4 +17,21 @@ public class Week02Application {
 		SpringApplication.run(Week02Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(CourseRepository repository) {
+		return (args) -> {
+			repository.findAll();
+			System.out.println("기존 저장 인쇄\n");
+
+			Course course = new Course("웹개발의 봄, Spring", "남병관");
+			repository.save(course);
+
+			System.out.println("새 저장 인쇄");
+			List<Course> courses = repository.findAll();
+			for (Course item: courses) {
+				System.out.println(item.getTitle());
+				System.out.println(item.getTutor());
+			}
+		};
+	}
 }
