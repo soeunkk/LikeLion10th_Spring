2ae4a2990fb87a575ccc8934bacc6e7ee92dca44
soeunkk <luckyber1@naver.com>
2022-08-21 21:43:48 +0900

    [2주차-숙제] initial project

---
+++ week02_hw/src/main/java/com/example/week02_hw/Week02HwApplication.java
@@ -0,0 +1,13 @@
+package com.example.week02_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week02HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week02HwApplication.class, args);
+	}
+
+}
