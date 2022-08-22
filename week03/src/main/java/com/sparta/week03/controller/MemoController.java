726a75a3a831e7e44124f36668df46c00cdbb297
soeunkk <luckyber1@naver.com>
2022-08-22 19:25:55 +0900

    [3주차] Memo API 오류 수정

--- week03/src/main/java/com/sparta/week03/controller/MemoController.java
+++ week03/src/main/java/com/sparta/week03/controller/MemoController.java
@@ -24,15 +24,15 @@ public class MemoController {
 	private final MemoRepository memoRepository;
 	private final MemoService memoService;
 
+	@GetMapping("/api/memos")
+	public List<Memo> getMemos() {
+		return memoRepository.findAllByOrderByModifiedAtDesc();
+	}
+
 	@PostMapping("/api/memos")
 	public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
 		Memo memo = new Memo(requestDto);
 		return memoRepository.save(memo);
-	}
-
-	@GetMapping("/api/memos")
-	public List<Memo> getMemos() {
-		return memoRepository.findAllByOrderByModifiedAtDesc();
 	}
 
 	@PutMapping("/api/memos/{id}")
--- week03/src/main/java/com/sparta/week03/domain/Timestamped.java
+++ week03/src/main/java/com/sparta/week03/domain/Timestamped.java
@@ -9,9 +9,12 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import lombok.Getter;
+
+@Getter	// for JSON 변환
 @MappedSuperclass	// Entity가 자동으로 컬럼으로 인식
 @EntityListeners(AuditingEntityListener.class)	// 생성/변경 시간을 자동으로 업데이트
-public class Timestamped {
+public abstract class Timestamped {	// 직접 사용할 일이 없으므로 추상 클래스(abstract)로 만들기
 
 	@CreatedDate
 	private LocalDateTime createdAt;
